665cc4d62cc355444d64f42aa61bc86f1f3cebc0
arass <arass@rpi.edu>
2021-03-12 09:32:27 -0500

    Switched to js
    Signed-off-by: arass <arass@rpi.edu>

--- commands/code.py
+++
@@ -1,3 +0,0 @@
-import discord
-from discord.ext.commands import Bot
-
--- utils/MessageTrigger.py
+++
@@ -1,6 +0,0 @@
-class MessageTrigger:
-    async def execute_message(self, client, msg):
-        raise NotImplementedError(
-            "'execute_message' is not implemented for this message trigger"
-        )
-
