e79e2db6eaf3d1e56f08b613b7d428f71a1d4f9b
arass <arass@rpi.edu>
2021-03-13 00:00:12 -0500

    added ton of quotes
    Signed-off-by: arass <arass@rpi.edu>

--- commands/quote.js
+++ commands/quote.js
@@ -2,17 +2,9 @@ const fs = require('fs');
 let rawdata = fs.readFileSync('./quotes/quotes.json');
 let data = JSON.parse(rawdata);
 
-
-const ricky_quotes = ["Fuck off dickweed!", "Where there is smoke there is wires!", "Two Turnips In Heat.", "Worst Case Ontario.", 
-               "Get two birds stoned at once", "It's Clear To See Who Makes The Pants Here."]
-const bubbles_quotes = ["That depends, can you go fuck yourself?", "I'm not running over a kitty to save your dope Ricky!", 
-                        "I Ain't Never Been So Frisky Before In My Whole Life!"];
-const julian_quotes = ["Did they just call me Patrick Swayze?"];
 function random_item(items){
         return items[Math.floor(Math.random()*items.length)];
     }
-
-
 
 module.exports = {
 
@@ -25,9 +17,13 @@ module.exports = {
             let random_ricky = random_item(data["quotes"]["Ricky"]);
             message.channel.send(`${random_ricky["text"]}`);
         }
-        else if(args[0] == "Bubbles")
-            message.channel.send(`${random_item(bubbles_quotes)}`);
-        else if(args[0] == "Julian")
-            message.channel.send(`${random_item(julian_quotes)}`);
+        else if(args[0] == "Bubbles"){
+            let random_bubbles = random_item(data["quotes"]["Bubbles"]);
+            message.channel.send(`${random_bubbles["text"]}`);
+        }
+        else if(args[0] == "Julian"){
+            let krandom_julian = random_item(data["quotes"]["Julian"]);
+            message.channel.send(`${random_julian["text"]}`);
+        }
 	},
 };
